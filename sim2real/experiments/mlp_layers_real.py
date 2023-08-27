56dedd8ed59901ff7daa34536a0bf913aa53e705
Jonas Scholz <jonas.scholz123@gmail.com>
2023-08-27 11:28:15 -0400

    Add mlp layers experiment

---
+++ sim2real/experiments/mlp_layers_real.py
@@ -0,0 +1,39 @@
+# %%
+from sim2real.config import (
+    DataSpec,
+    ModelSpec,
+    OptimSpec,
+    OutputSpec,
+    Paths,
+    TuneSpec,
+    TunerType,
+    tune_opt as opt,
+    out,
+    model,
+    data,
+    names,
+    paths,
+    tune,
+)
+
+from sim2real.train.tune import Sim2RealTrainer
+
+from dataclasses import replace
+
+mlp_capacity = 128
+nums_mlp_layers = [1]
+
+# No pretraining.
+exp_opt = replace(opt, start_from=None)
+
+# Maximum number of real data.
+exp_tune = replace(tune, num_stations=500, num_tasks=10000, no_pretraining=True)
+
+
+for num_mlp_layers in nums_mlp_layers:
+    mlp_channels = (mlp_capacity,) * num_mlp_layers
+    exp_model = replace(model, aux_t_mlp_layers=mlp_channels, dim_aux_t=0)
+    trainer = Sim2RealTrainer(paths, exp_opt, out, data, exp_model, exp_tune)
+    trainer.train()
+
+# %%
